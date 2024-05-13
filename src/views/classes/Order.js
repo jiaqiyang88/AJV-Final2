3ab8a61509c2b2e2298b9a9111eafd5caaad0eb2
jiaqiyang88 <168867015+jiaqiyang88@users.noreply.github.com>
2024-05-12 19:32:14 -0700

    strings

--- src/views/classes/Order.js
+++ src/views/classes/Order.js
@@ -34,13 +34,13 @@ class Order {
         var diffDays = Math.floor(Math.abs((this.orderDate.getTime() - now.getTime())/(oneDay))); //$NON-NLS-L$
 
         if(diffDays < 2) {
-            return "Processing";
+            return Processing;
         }
         if(diffDays < 4) {
-            return "Shipped";
+            return Shipped;
         }
         else{
-            return "Delivered";
+            return Delivered;
         }
     } 
 }
